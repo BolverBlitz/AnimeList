f4d33a70cf663c5741e27de0d58d220f31bab5bd
BolverBlitz <bolverblitz@ebg.pw>
2023-12-11 12:02:14 +0100

    Added worldbuilding metric

--- tools/AddAnime.js
+++ tools/AddAnime.js
@@ -45,12 +45,13 @@ async function Questions() {
     const rating_story = await askQuestion("Rating Story (1-100): ");
     const rating_values = await askQuestion("Rating Values (1-100): ");
     const rating_character = await askQuestion("Rating Character (1-100): ");
+    const rating_worldbuilding = await askQuestion("Rating Worldbuilding (1-100): ");
 
     // Convert to Date
     const watch_start = convertToTimestamp(watch_start_date);
     const watch_end = convertToTimestamp(watch_end_data);
 
-    const sql_response = await Anime.Create.Anime(animename, language, season, episodes, rating_grafik, rating_story, rating_values, rating_character, watch_start, watch_end);
+    const sql_response = await Anime.Create.Anime(animename, language, season, episodes, rating_grafik, rating_story, rating_values, rating_character, rating_worldbuilding, watch_start, watch_end);
     if (!sql_response) throw new DBError('Anime.Create.Anime', 1, typeof 1, sql_response, typeof sql_response);
 
     console.log("Anime created!");
